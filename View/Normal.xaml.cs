7039d721072b122f9da01f8ec016b8474210d28e
Wei <144542224+1105623876@users.noreply.github.com>
2025-05-12 20:49:54 +0800

    Normal代替UserWindow

---
+++ View/Normal.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Csharp_final_assignment_Face_Recognition_Attendance_System.Business;
+using Csharp_final_assignment_Face_Recognition_Attendance_System.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static Csharp_final_assignment_Face_Recognition_Attendance_System.Core.Models;
+
+namespace Csharp_final_assignment_Face_Recognition_Attendance_System.View
+{
+    /// <summary>
+    /// Normal.xaml 的交互逻辑
+    /// </summary>
+    public partial class Normal : UserControl
+    {
+        public Normal()
+        {
+            InitializeComponent();
+        }
+    }
+}
