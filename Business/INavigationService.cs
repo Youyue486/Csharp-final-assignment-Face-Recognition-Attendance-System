ec20c7382b1980feb59ced254930a8cf14c05896
Youyue486 <asideeeee@gmail.com>
2025-05-30 19:13:21 +0800

    Update INavigationService.cs

--- Business/INavigationService.cs
+++ Business/INavigationService.cs
@@ -1,4 +1,4 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +9,8 @@ namespace Csharp_final_assignment_Face_Recognition_Attendance_System.Business
 {
     public interface INavigationService
     {
-        void NavigateTo<TViewModel>() where TViewModel : ObservableObject;
+        public void NavigateTo<T>();
+        public event Action CurrentViewModelChanged;
+        ObservableObject CurrentViewModel { get; set; }
     }
 }
